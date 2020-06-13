e4cff210529fdf9ae9245bedd915f9230032aed0
Emmanuel Isi <50160339+Emmanuelisi20@users.noreply.github.com>
2020-06-13 12:33:45 -0400

    Add files via upload
    Added new file

---
+++ php reg.php
@@ -0,0 +1,31 @@
+<!--
+<form action = "registration_page.php">
+<div class = "containers" >
+  <h1> Register </h1>
+  <hr>
+  <label for = "USERNAME">
+  <b>Username</b>
+  </label>
+  <input type = "text" placeholder = "Enter Username"
+  name = "USERNAME" required>
+
+  <label for = "email">
+  <b> Email </b>
+  </label>
+  <input type = "text" placeholder = "Enter Email"
+  name = "Email" required>
+
+  <label for = "Password">
+  <b> Password </b>
+  </label>
+  <input type = "password" placeholder = "Enter Password"
+  name = "Password" required>
+
+  <label for = "Again_Password">
+  <b> Repeat Password </b>
+  </label>
+  <input type = "password" placeholder = "Enter Password Again"
+  name = "Password" required>
+  </hr>
+</form>
+-->
